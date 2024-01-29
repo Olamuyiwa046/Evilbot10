403b59f54842194f402f80c7ff132fefde362e44
Olamuyiwa046 <151450993+Olamuyiwa046@users.noreply.github.com>
2024-01-29 11:48:09 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,31 +12,31 @@ const chalk = require('chalk')
 
 //owmner v card
 global.ytname = "YT: JusticeTech" //ur yt chanel name
-global.socialm = "GitHub: JusticeTechBot" //ur github or insta name
-global.location = "Nigeria, Edo, Benin" //ur location
+global.socialm = "GitHub: iamevilboy" //ur github or insta name
+global.location = "Nigeria, lagos, abuja" //ur location
 
 //new
-global.botname = 'JusticeTechBot' //ur bot name
-global.ownernumber = 2349032578690' //ur owner number
-global.ownername = 'JusticeTech' //ur owner name
+global.botname = '🎯𝐁𝐋𝐔𝐄 𝐈𝐂𝐎𝐍—𝐁𝐎𝐓🎯' //ur bot name
+global.ownernumber = 2348122716856' //ur owner number
+global.ownername = '🎯𝐁𝐋𝐔𝐄 𝐈𝐂𝐎𝐍🎯' //ur owner name
 global.websitex = "http://www.youtube.com/@JusticeTech"
-global.wagc = "https://chat.whatsapp.com/GdEqRb5g5BOLNcjUKvcqLY"
-global.themeemoji = '🪀'
-global.wm = "JusticeTechBot Inc."
+global.wagc = "https://chat.whatsapp.com/HQ26izcpZ7yAWXm2yyExSX"
+global.themeemoji = '🎯'
+global.wm = "Antisocialbot Inc."
 global.botscript = 'https://github.com/JusticeTechBot/JusticeTech-Bot' //script link
 global.packname = "Sticker By"
-global.author = "JusticeTech\n\n+2349032578690"
-global.creator = "2349032578690@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["2349032578690"] // Premium User
+global.author = "JusticeTech\n\n"
+global.creator = "2348122716856@s.whatsapp.net"
+global.xprefix = ','
+global.premium = ["2348122716856"] // Premium User
 global.hituet = 0
 
 //bot sett
 global.typemenu = 'v8' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
-global.autoblocknumber = '92' //set autoblock country code
+global.autoblocknumber = '212' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
+global.welcome = true //welcome/left in groups
 global.anticall = false //bot blocks user when called
 global.autoswview = false //auto status/story view
 global.adminevent = false //show promote/demote message
@@ -47,7 +47,7 @@ global.mess = {
 	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
     done: 'Done✓',
     error: 'Error!',
-    success: 'Here you go!'
+    success: 'Here you go🎯!'
 }
 //thumbnail
 global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
